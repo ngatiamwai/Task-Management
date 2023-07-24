3f81b6cd0cb52ce2a356c259e6c44367fcdf7a91
ngatiamwai <ngatiamwai25@gmail.com>
2023-07-24 09:36:13 +0300

    Updated the main.js

--- main.js
+++ main.js
@@ -1,252 +1,207 @@
-
 class CompletedTodo {
-    constructor(title, description, deadline, completedOn) {
-      this.title = title;
-      this.description = description;
-      this.deadline = deadline;
-    //   this.completedOn = completedOn;
-    }
+  constructor(title, description, deadline, completedOn) {
+    this.title = title;
+    this.description = description;
+    this.deadline = deadline;
+    this.completedOn = completedOn;
   }
-  
-  class UncompletedTodo {
-    constructor(title, description, deadline) {
-      this.title = title;
-      this.description = description;
-      this.deadline = deadline;
-    }
+}
+
+class UncompletedTodo {
+  constructor(title, description, deadline) {
+    this.title = title;
+    this.description = description;
+    this.deadline = deadline;
   }
-  
-  const input_tasks = document.getElementById('input_tasks');
-  const tasks_table = document.getElementById('tasks_table');
-  const uncompletedTasks = [];
-  const completedTasks = [];
+}
 
-  localStorage.setItem('tasks_table',JSON.stringify(tasks_table));
-  localStorage.setItem('input_tasks',JSON.stringify(input_tasks));
-  
-// console.log('tasks_table', JSON.stringify(tasks_table));
+let completedTasks = [];
+let uncompletedTasks = [];
 
-//   function renderTasks() {
-//     tasksContainer.innerHTML = '';
-  
-//     // Render uncompleted tasks
-//     uncompletedTasks.forEach((task) => {
-//       const taskItem = createTaskElement(task);
-//       tasksContainer.appendChild(taskItem);
-//     });
-  
-//     // Render completed tasks
-//     completedTasks.forEach((task) => {
-//       const taskItem = createTaskElement(task);
-//       tasksContainer.appendChild(taskItem);
-//     });
-//   }
-  
-//   function createTaskElement(task) {
-//     const taskItem = document.createElement('div');
-//     taskItem.className = 'task-item';
-  
-//     const titleElem = document.createElement('h3');
-//     titleElem.textContent = task.title;
-//     taskItem.appendChild(titleElem);
-  
-//     const descriptionElem = document.createElement('p');
-//     descriptionElem.textContent = task.description;
-//     taskItem.appendChild(descriptionElem);
-  
-//     const deadlineElem = document.createElement('p');
-//     deadlineElem.textContent = 'Deadline: ' + task.deadline;
-//     taskItem.appendChild(deadlineElem);
-  
-//     if (task instanceof CompletedTodo) {
-//       const completedOnElem = document.createElement('p');
-//       completedOnElem.textContent = 'Completed On: ' + task.completedOn;
-//       taskItem.appendChild(completedOnElem);
-//     } else {
-//       const completeBtn = document.createElement('button');
-//       completeBtn.textContent = 'Complete';
-//       completeBtn.addEventListener('click', () => completeTask(task));
-//       taskItem.appendChild(completeBtn);
-//     }
-  
-//     const deleteBtn = document.createElement('button');
-//     deleteBtn.textContent = 'Delete';
-//     deleteBtn.addEventListener('click', () => deleteTask(task));
-//     taskItem.appendChild(deleteBtn);
-  
-//     return taskItem;
-//   }
-  
-//   function addTask(title, description, deadline) {
-//     const newTask = new UncompletedTodo(title, description, deadline);
-//     uncompletedTasks.push(newTask);
-//     renderTasks();
-//   }
-  
-//   function completeTask(task) {
-//     const completedOn = new Date().toLocaleDateString();
-//     const completedTask = new CompletedTodo(
-//       task.title,
-//       task.description,
-//       task.deadline,
-//       completedOn
-//     );
-//     completedTasks.push(completedTask);
-//     uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
-//     renderTasks();
-//   }
-  
-//   function deleteTask(task) {
-//     if (task instanceof CompletedTodo) {
-//       completedTasks.splice(completedTasks.indexOf(task), 1);
-//     } else {
-//       uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
-//     }
-//     renderTasks();
-//   }
-  
-//   taskForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const title = e.target.elements.title.value;
-//     const description = e.target.elements.description.value;
-//     const deadline = e.target.elements.deadline.value;
-//     addTask(title, description, deadline);
-//     e.target.reset();
-//   });
-  
-//   renderTasks();
-  
+const taskForm = document.createElement('form');
+taskForm.id = 'taskForm';
+taskForm.innerHTML = `
+  <input type="text" id="title" placeholder="Title" required>
+  <input type="text" id="description" placeholder="Description" required>
+  <input type="date" id="deadline" required>
+  <button type="submit">Add Task</button>
+`;
 
-// class CompletedTodo {
-//     constructor(title, description, deadline, completedOn) {
-//       this.title = title;
-//       this.description = description;
-//       this.deadline = deadline;
-//       this.completedOn = completedOn;
-//     }
-//   }
+const tasksContainer = document.createElement('div');
+tasksContainer.id = 'tasks';
+
+function saveTasksToLocalStorage() {
+  localStorage.setItem('uncompletedTasks', JSON.stringify(uncompletedTasks));
+  localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+}
+
+function getTasksFromLocalStorage() {
+  uncompletedTasks = (JSON.parse(localStorage.getItem('uncompletedTasks')) || []).map(taskData => new UncompletedTodo(...Object.values(taskData)));
+  completedTasks = (JSON.parse(localStorage.getItem('completedTasks')) || []).map(taskData => new CompletedTodo(...Object.values(taskData)));
+}
+
+function renderTasks() {
+  tasksContainer.innerHTML = '';
+
+  // Render uncompleted tasks
+  uncompletedTasks.forEach(task => tasksContainer.appendChild(createTaskElement(task)));
+
+  // Render completed tasks
+  completedTasks.forEach(task => tasksContainer.appendChild(createTaskElement(task)));
+
+  // Save tasks to local storage
+  saveTasksToLocalStorage();
+}
+
+function createTaskElement(task) {
+  const taskTemplate = document.getElementById('taskTemplate');
+  const taskItem = taskTemplate.content.cloneNode(true);
   
-//   class UncompletedTodo {
-//     constructor(title, description, deadline) {
-//       this.title = title;
-//       this.description = description;
-//       this.deadline = deadline;
-//     }
-//   }
-  
-//   const taskForm = document.getElementById('taskForm');
-//   const tasksContainer = document.getElementById('tasks');
-//   let uncompletedTasks = [];
-//   let completedTasks = [];
-  
-//   function saveTasksToLocalStorage() {
-//     localStorage.setItem('uncompletedTasks', JSON.stringify(uncompletedTasks));
-//     localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
-//   }
-  
-//   function getTasksFromLocalStorage() {
-//     const uncompletedTasksData = JSON.parse(localStorage.getItem('uncompletedTasks')) || [];
-//     const completedTasksData = JSON.parse(localStorage.getItem('completedTasks')) || [];
-  
-//     uncompletedTasks = uncompletedTasksData.map(taskData => new UncompletedTodo(taskData.title, taskData.description, taskData.deadline));
-//     completedTasks = completedTasksData.map(taskData => new CompletedTodo(taskData.title, taskData.description, taskData.deadline, taskData.completedOn));
-//   }
-  
-//   function renderTasks() {
-//     tasksContainer.innerHTML = '';
-  
-//     // Render uncompleted tasks
-//     uncompletedTasks.forEach((task) => {
-//       const taskItem = createTaskElement(task);
-//       tasksContainer.appendChild(taskItem);
-//     });
-  
-//     // Render completed tasks
-//     completedTasks.forEach((task) => {
-//       const taskItem = createTaskElement(task);
-//       tasksContainer.appendChild(taskItem);
-//     });
-  
-//     // Save tasks to local storage
-//     saveTasksToLocalStorage();
-//   }
-  
-//   function createTaskElement(task) {
-//     // ... (no change in this function)
-//     const taskItem = document.createElement('div');
-//     taskItem.className = 'task-item';
-  
-//     const titleElem = document.createElement('h3');
-//     titleElem.textContent = task.title;
-//     taskItem.appendChild(titleElem);
-  
-//     const descriptionElem = document.createElement('p');
-//     descriptionElem.textContent = task.description;
-//     taskItem.appendChild(descriptionElem);
-  
-//     const deadlineElem = document.createElement('p');
-//     deadlineElem.textContent = 'Deadline: ' + task.deadline;
-//     taskItem.appendChild(deadlineElem);
-  
-//     if (task instanceof CompletedTodo) {
-//       const completedOnElem = document.createElement('p');
-//       completedOnElem.textContent = 'Completed On: ' + task.completedOn;
-//       taskItem.appendChild(completedOnElem);
-//     } else {
-//       const completeBtn = document.createElement('button');
-//       completeBtn.textContent = 'Complete';
-//       completeBtn.addEventListener('click', () => completeTask(task));
-//       taskItem.appendChild(completeBtn);
-//     }
-  
-//     const deleteBtn = document.createElement('button');
-//     deleteBtn.textContent = 'Delete';
-//     deleteBtn.addEventListener('click', () => deleteTask(task));
-//     taskItem.appendChild(deleteBtn);
-  
-//     return taskItem;
-//   }
-  
-//   function addTask(title, description, deadline) {
-//     const newTask = new UncompletedTodo(title, description, deadline);
-//     uncompletedTasks.push(newTask);
-//     renderTasks();
-//   }
-  
-//   function completeTask(task) {
-//     const completedOn = new Date().toLocaleDateString();
-//     const completedTask = new CompletedTodo(
-//       task.title,
-//       task.description,
-//       task.deadline,
-//       completedOn
-//     );
-//     completedTasks.push(completedTask);
-//     uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
-//     renderTasks();
-//   }
-  
-//   function deleteTask(task) {
-//     if (task instanceof CompletedTodo) {
-//       completedTasks.splice(completedTasks.indexOf(task), 1);
-//     } else {
-//       uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
-//     }
-//     renderTasks();
-//   }
-  
-//   taskForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const title = e.target.elements.title.value;
-//     const description = e.target.elements.description.value;
-//     const deadline = e.target.elements.deadline.value;
-//     addTask(title, description, deadline);
-//     e.target.reset();
-//   });
-  
-//   //Load tasks from local storage on page load
-//   getTasksFromLocalStorage();
-  
-//   // Initial render of tasks
-//   renderTasks();
-  
+  taskItem.querySelector('.task-title').textContent = task.title;
+  taskItem.querySelector('.task-description').textContent = task.description;
+  taskItem.querySelector('.task-deadline').textContent = 'Deadline: ' + task.deadline;
+
+  if (task instanceof CompletedTodo) {
+    taskItem.querySelector('.task-completedOn').textContent = 'Completed On: ' + task.completedOn;
+  } else {
+    const completeBtn = taskItem.querySelector('.completeBtn');
+    completeBtn.addEventListener('click', () => completeTask(task));
+    completeBtn.style.display = 'block';
+  }
+
+  const editBtn = taskItem.querySelector('.editBtn');
+  editBtn.addEventListener('click', () => editTask(task));
+
+  const deleteBtn = taskItem.querySelector('.deleteBtn');
+  deleteBtn.addEventListener('click', () => deleteTask(task));
+
+  return taskItem;
+}
+
+function editTask(task) {
+  const newTitle = prompt('Enter new title:', task.title);
+  const newDescription = prompt('Enter new description:', task.description);
+  const newDeadline = prompt('Enter new deadline:', task.deadline);
+
+  if (newTitle !== null && newTitle !== '') {
+    task.title = newTitle;
+  }
+
+  if (newDescription !== null && newDescription !== '') {
+    task.description = newDescription;
+  }
+
+  if (newDeadline !== null && newDeadline !== '') {
+    task.deadline = newDeadline;
+  }
+
+  renderTasks();
+}
+
+const buttons = document.querySelectorAll('.buttons button');
+buttons.forEach(button => {
+  button.addEventListener('click', () => filterTasks(button.getAttribute('data-filter')));
+});
+
+function addTask(title, description, deadline) {
+  uncompletedTasks.push(new UncompletedTodo(title, description, deadline));
+  renderTasks();
+}
+
+function completeTask(task) {
+  const completedOn = new Date().toLocaleDateString();
+  completedTasks.push(new CompletedTodo(task.title, task.description, task.deadline, completedOn));
+  uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
+  renderTasks();
+}
+
+function deleteTask(task) {
+  task instanceof CompletedTodo
+    ? completedTasks.splice(completedTasks.indexOf(task), 1)
+    : uncompletedTasks.splice(uncompletedTasks.indexOf(task), 1);
+  renderTasks();
+}
+
+taskForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const { title, description, deadline } = e.target.elements;
+  addTask(title.value, description.value, deadline.value);
+  e.target.reset();
+});
+
+document.body.appendChild(taskForm);
+document.body.appendChild(tasksContainer);
+
+// Load tasks from local storage on page load
+getTasksFromLocalStorage();
+
+// ... (previous code remains unchanged)
+
+const allButton = document.createElement('button');
+allButton.textContent = 'All';
+allButton.setAttribute('data-filter', 'all');
+allButton.addEventListener('click', () => filterTasks('all'));
+
+const completedButton = document.createElement('button');
+completedButton.textContent = 'Completed';
+completedButton.setAttribute('data-filter', 'completed');
+completedButton.addEventListener('click', () => filterTasks('completed'));
+
+const notCompletedButton = document.createElement('button');
+notCompletedButton.textContent = 'Not Completed';
+notCompletedButton.setAttribute('data-filter', 'not_completed');
+notCompletedButton.addEventListener('click', () => filterTasks('not_completed'));
+
+const overdueButton = document.createElement('button');
+overdueButton.textContent = 'Overdue';
+overdueButton.setAttribute('data-filter', 'overdue');
+overdueButton.addEventListener('click', () => filterTasks('overdue'));
+
+const buttonsContainer = document.createElement('div');
+buttonsContainer.className = 'buttons';
+buttonsContainer.append(allButton, completedButton, notCompletedButton, overdueButton);
+
+document.body.insertBefore(buttonsContainer, taskForm);
+
+function isTaskOverdue(task) {
+  const today = new Date();
+  const deadline = new Date(task.deadline);
+  return task instanceof UncompletedTodo && today > deadline;
+}
+
+
+function filterTasks(filterType) {
+  let filteredTasks = [];
+
+  switch (filterType) {
+    case 'all':
+      filteredTasks = uncompletedTasks.concat(completedTasks);
+      break;
+    case 'completed':
+      filteredTasks = completedTasks;
+      break;
+    case 'not_completed':
+      filteredTasks = uncompletedTasks;
+      break;
+    case 'overdue':
+      const overdueUncompletedTasks = uncompletedTasks.filter((task) => isTaskOverdue(task));
+      const overdueCompletedTasks = completedTasks.filter((task) => isTaskOverdue(task));
+      filteredTasks = overdueUncompletedTasks.concat(overdueCompletedTasks);
+      break;
+    default:
+      break;
+  }
+
+  tasksContainer.innerHTML = '';
+
+  filteredTasks.forEach((task) => {
+    const taskItem = createTaskElement(task);
+    tasksContainer.appendChild(taskItem);
+  });
+}
+
+// Initial render of tasks
+getTasksFromLocalStorage();
+filterTasks('all');
+
+// ... (remaining code remains unchanged)
